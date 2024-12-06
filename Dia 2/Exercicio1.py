fc5d003de4e6be14e69a133cf881e09086ec653c
LuizDev <102246451+LuizDCarvalho@users.noreply.github.com>
2024-12-06 13:13:09 -0300

    Update and rename Exercicio2.py to Exercicio1.py

---
+++ Dia 2/Exercicio1.py
@@ -0,0 +1,13 @@
+""" Atividade 01 - Escreva um programa que receba 
+dois números do usuario e imprima a soma deles. """
+
+
+# Solicita ao usuário que insira dois números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma
+result = num1 + num2
+
+# Exibe o resultado formatado
+print("O resultado da soma é: ", result)
