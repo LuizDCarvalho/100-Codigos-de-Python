2825f8ca9586385ea7fcb0f7ca00d82dc24583fe
LuizDev <102246451+LuizDCarvalho@users.noreply.github.com>
2025-01-10 11:42:50 -0300

    Add files via upload

---
+++ Dia 4/Exercicio4.py
@@ -0,0 +1,17 @@
+"""Escreva um programa que calcule o fatorial de um número."""
+
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado = resultado * i
+        return resultado
+    
+num = int(input("Digite um número:" ))
+
+if num < 0:
+    print(f"O fatorial de {num} não existe")
+else:
+    print(f"O fatorial de {num} é {fatorial(num)}")
